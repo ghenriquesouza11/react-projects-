5fa4b7eee0f94348088841bb4741be3a944e9c2d
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-03-26 01:37:18 -0300

    Projeto 01-birthday-reminder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [persons, setPersons] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{`${persons.length} Birthdays Today`}</h3>
+        <List arrayList={persons} />
+        <button type='button' onClick={ () => {
+          setPersons([]);
+        } } >
+            Clear All
+          </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const List = () => {
+const List = (props) => {
+  const { arrayList } = props;
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <section>
+      {arrayList.map((element) => (
+        <article key={element.id} className='person'>
+          <img src={element.image} alt='person-img' />
+          <div>
+            <h4>{ element.name }</h4>
+            <p>{`${element.age} years`}</p>
+          </div>
+        </article>
+      ))}
+    </section>
   );
 };
 

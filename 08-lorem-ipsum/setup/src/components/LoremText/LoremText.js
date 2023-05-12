3661825954c6d1520cbc1b7058ad1ef698c17a7f
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-05-12 19:02:35 -0300

    Estiliza os paragráfos gerados

--- 08-lorem-ipsum/setup/src/components/LoremText/LoremText.js
+++ 08-lorem-ipsum/setup/src/components/LoremText/LoremText.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 
 import data from '../../data';
 
+import './LoremText.css';
+
 function LoremText(props) {
   const { paragraphs } = props;
   return (
-    <div>
+    <article className="lorem-text">
       {
         data.slice(0, paragraphs).map((paragraph, index) => (
-          <p key={ index }>
+          <p
+            className="lorem-paragraph"
+            key={ index }
+          >
             {paragraph}
           </p>
         ))
       }
-    </div>
+    </article>
   );
 }
 

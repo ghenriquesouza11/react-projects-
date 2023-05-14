e40a429a822f73ef9dacd1cf4f91c871f78b1378
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-05-14 02:08:22 -0300

    Implementa a lógica para copiar a cor selecionada ao clicar no botão de copiar

--- 09-color-generator/setup/src/components/SingleColor/SingleColor.js
+++ 09-color-generator/setup/src/components/SingleColor/SingleColor.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import copy from 'copy-to-clipboard';
+
 import Copy from '../../svgs/Copy';
 
 import './SingleColor.css';
@@ -8,19 +10,27 @@ import './SingleColor.css';
 function SingleColor(props) {
   const { color, weight, type } = props;
   const verifyType = type === 'shade';
+
+  const handleCopyButton = () => {
+    copy(color);
+  };
   return (
     <article
       className="single-color"
       style={ { backgroundColor: color, color: verifyType ? 'white' : 'black' } }
     >
       <div className="color-info">
-        <p>{ `${weight} ${color}` }</p>
-        <button className="copy-button">
+        <p>{ weight }</p>
+        <button
+          onClick={ () => handleCopyButton() }
+          className="copy-button"
+        >
           <Copy
             stroke={ verifyType ? 'white' : 'black' }
           />
         </button>
       </div>
+      <p>{ color }</p>
     </article>
   );
 }
